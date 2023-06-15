106f2877679fc32942165041a4ed88749e45a618
SergeyVolik <volik.sergey112@gmail.com>
2023-06-15 17:10:51 +0300

    :recycle: first person input system

--- Assets/Rival_StandardCharacters/FirstPerson/Scripts/FirstPersonPlayerSystem.cs
+++ Assets/Rival_StandardCharacters/FirstPerson/Scripts/FirstPersonPlayerSystem.cs
@@ -20,6 +20,64 @@ public partial class FirstPersonPlayerSystem : SystemBase
         FixedUpdateTickSystem = World.GetExistingSystemManaged<FixedUpdateTickSystem>();
     }
 
+    [BurstCompile]
+    public partial struct FirstPersonPlayerJob : IJobEntity
+    {
+
+        public float2 lookInput;
+        public float2 moveInput;
+        public bool jumpInput;
+        public ComponentLookup<FirstPersonCharacterInputs> firstPersonCharacterInputs;
+
+        [ReadOnly]
+        public ComponentLookup<LocalToWorld> localToWorld;
+
+        [ReadOnly]
+        public ComponentLookup<FirstPersonCharacterComponent> firstPersonCharacterComponent;
+        public uint fixedTick;
+
+        [BurstCompile]
+        public void Execute(ref FirstPersonPlayer player)
+        {
+            if (firstPersonCharacterInputs.HasComponent(player.ControlledCharacter) && firstPersonCharacterComponent.HasComponent(player.ControlledCharacter))
+            {
+                var characterInputs = firstPersonCharacterInputs.GetRefRW(player.ControlledCharacter);
+             
+
+                var controllerLocalTransform = localToWorld.GetRefRO(player.ControlledCharacter);
+             
+
+                quaternion characterRotation = controllerLocalTransform.ValueRO.Rotation;
+              
+
+                // Look
+                characterInputs.ValueRW.LookYawPitchDegrees = lookInput * player.RotationSpeed;
+
+                // Move
+                float3 characterForward = math.mul(characterRotation, math.forward());
+                float3 characterRight = math.mul(characterRotation, math.right());
+                characterInputs.ValueRW.MoveVector = (moveInput.y * characterForward) + (moveInput.x * characterRight);
+                characterInputs.ValueRW.MoveVector = Rival.MathUtilities.ClampToMaxLength(characterInputs.ValueRO.MoveVector, 1f);
+
+                // Jump
+                // Punctual input presses need special handling when they will be used in a fixed step system.
+                // We essentially need to remember if the button was pressed at any point over the last fixed update
+                if (player.LastInputsProcessingTick == fixedTick)
+                {
+                    characterInputs.ValueRW.JumpRequested = jumpInput || characterInputs.ValueRO.JumpRequested;
+                }
+                else
+                {
+                    characterInputs.ValueRW.JumpRequested = jumpInput;
+                }
+
+                player.LastInputsProcessingTick = fixedTick;
+
+
+            }
+        }
+    }
+
     protected override void OnUpdate()
     {
         uint fixedTick = FixedUpdateTickSystem.FixedTick;
@@ -33,45 +91,18 @@ public partial class FirstPersonPlayerSystem : SystemBase
         bool jumpInput = Input.GetKeyDown(KeyCode.Space);
         float2 lookInput = new float2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
-        // Iterate on all Player components to apply input to their character
-        Entities
-            .ForEach((ref FirstPersonPlayer player) =>
-            {
-                if (HasComponent<FirstPersonCharacterInputs>(player.ControlledCharacter) && HasComponent<FirstPersonCharacterComponent>(player.ControlledCharacter))
-                {
-                    FirstPersonCharacterInputs characterInputs = GetComponent<FirstPersonCharacterInputs>(player.ControlledCharacter);
-                    FirstPersonCharacterComponent character = GetComponent<FirstPersonCharacterComponent>(player.ControlledCharacter);
 
-                    var controllerLocalTransform = GetComponent<LocalToWorld>(player.ControlledCharacter);
-                    var characterViewLocalTransfrom = GetComponent<LocalToWorld>(character.CharacterViewEntity);
-                    quaternion characterRotation = controllerLocalTransform.Rotation;
-                    quaternion localCharacterViewRotation = characterViewLocalTransfrom.Rotation;
+        Dependency = new FirstPersonPlayerJob
+        {
+            fixedTick = fixedTick,
+            moveInput = moveInput,
+            jumpInput = jumpInput,
+            lookInput = lookInput,
+            firstPersonCharacterInputs = SystemAPI.GetComponentLookup<FirstPersonCharacterInputs>(),
+            firstPersonCharacterComponent = SystemAPI.GetComponentLookup<FirstPersonCharacterComponent>(isReadOnly: true),
+            localToWorld = SystemAPI.GetComponentLookup<LocalToWorld>(),
 
-                    // Look
-                    characterInputs.LookYawPitchDegrees = lookInput * player.RotationSpeed;
+        }.Schedule(Dependency);
 
-                    // Move
-                    float3 characterForward = math.mul(characterRotation, math.forward());
-                    float3 characterRight = math.mul(characterRotation, math.right());
-                    characterInputs.MoveVector = (moveInput.y * characterForward) + (moveInput.x * characterRight);
-                    characterInputs.MoveVector = Rival.MathUtilities.ClampToMaxLength(characterInputs.MoveVector, 1f);
-
-                    // Jump
-                    // Punctual input presses need special handling when they will be used in a fixed step system.
-                    // We essentially need to remember if the button was pressed at any point over the last fixed update
-                    if (player.LastInputsProcessingTick == fixedTick)
-                    {
-                        characterInputs.JumpRequested = jumpInput || characterInputs.JumpRequested;
-                    }
-                    else
-                    {
-                        characterInputs.JumpRequested = jumpInput;
-                    }
-
-                    player.LastInputsProcessingTick = fixedTick;
-
-                    SetComponent(player.ControlledCharacter, characterInputs);
-                }
-            }).Schedule();
     }
 }
